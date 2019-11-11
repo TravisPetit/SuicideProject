1b217e16604cbeca48a700bc333a2f79081da1eb
TravisPetit <travis.riverapetit@protonmail.com>
2019-11-11 14:56:20 +0100

    Create files directory

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
-import requests, zipfile, io, shutil
+import requests, zipfile, io, shutil, os
 
+VERBOSE = True
 DESTINATION = "./files"
+os.mkdir(DESTINATION)
 
 data = open("files.txt", "r")
 
@@ -15,8 +17,8 @@ for line in data:
     filename = url.split("v2/")
     filename = filename[-1].strip()
     filename = filename[0:-4] #remove .zip
-    print(filename)
 
-    #to = DESTINATION + filename
-    #shutil.move("./" + filename, to)
+    if VERBOSE:
+        print(filename)
+
     shutil.move("./" + filename, DESTINATION)
