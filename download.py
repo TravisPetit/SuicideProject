b7ae5a1c066bf3c9eddcf56b44b7999073f531a9
TravisPetit <travis.riverapetit@protonmail.com>
2019-11-12 09:05:41 +0100

    Only print errors if verbose

--- download.py
+++ download.py
@@ -14,14 +14,15 @@ for line in data:
     words = line.split(" ")
     url = words[-1].strip()
 
+    filename = url.split("v2/")
+    filename = filename[-1].strip()
+    filename = filename[0:-4] #remove .zip
+
     try:
         req = requests.get(url, stream=True)
+
         z = zipfile.ZipFile(io.BytesIO(req.content))
         z.extractall()
-
-        filename = url.split("v2/")
-        filename = filename[-1].strip()
-        filename = filename[0:-4] #remove .zip
 
         shutil.move("./" + filename, DESTINATION)
 
@@ -30,13 +31,14 @@ for line in data:
         mssg += "--\n"
         mssg += "Exception instance: {}\nException arguments: {}\nException string: {}\n".format(type(e),e.args,e)
         mssg += "--"
+
+        if VERBOSE: print(mssg)
+
         f = open("bad_indices.txt", "a+")
         f.write(str(i)+",")
         f.close()
 
-        print(mssg)
-
     if VERBOSE and (i%10 == 0):
-        print( str(i) + " / " + str(LEN) )
+        print(str(i) + " / " + str(LEN))
 
 data.close()
