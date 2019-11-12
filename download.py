594da962b7f04698c7fcf1b0b3727fc94f166590
TravisPetit <travis.riverapetit@protonmail.com>
2019-11-12 08:55:31 +0100

    Add try catch

--- download.py
+++ download.py
@@ -4,10 +4,8 @@ VERBOSE = True
 DESTINATION = "./files"
 LEN = 163536
 
-
 os.mkdir(DESTINATION)
 data = open("files.txt", "r")
-
 
 i = 0
 for line in data:
@@ -16,15 +14,29 @@ for line in data:
     words = line.split(" ")
     url = words[-1].strip()
 
-    req = requests.get(url, stream=True)
-    z = zipfile.ZipFile(io.BytesIO(req.content))
-    z.extractall()
+    try:
+        req = requests.get(url, stream=True)
+        z = zipfile.ZipFile(io.BytesIO(req.content))
+        z.extractall()
 
-    filename = url.split("v2/")
-    filename = filename[-1].strip()
-    filename = filename[0:-4] #remove .zip
+        filename = url.split("v2/")
+        filename = filename[-1].strip()
+        filename = filename[0:-4] #remove .zip
 
-    shutil.move("./" + filename, DESTINATION)
+        shutil.move("./" + filename, DESTINATION)
+
+    except Exception as e:
+        mssg = "Skipping file: {}, iteration: {}\n".format(filename, i)
+        mssg += "--\n"
+        mssg += "Exception instance: {}\nException arguments: {}\nException string: {}\n".format(type(e),e.args,e)
+        mssg += "--"
+        f = open("bad_indices.txt", "a+")
+        f.write(str(i)+",")
+        f.close()
+
+        print(mssg)
 
     if VERBOSE and (i%10 == 0):
         print( str(i) + " / " + str(LEN) )
+
+data.close()
