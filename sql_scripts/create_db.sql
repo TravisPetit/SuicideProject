42bd900692bf868b6c030377dec7a227335117cf
overactor <rik.degraaff@gmx.ch>
2019-11-27 12:29:58 +0100

    added database creation script

---
+++ sql_scripts/create_db.sql
@@ -0,0 +1,99 @@
+CREATE TABLE 'Action' (
+    id SERIAL PRIMARY KEY,
+    eventCode VARCHAR(255) NOT NULL,
+    eventRootCode VARCHAR(255) NOT NULL,
+    eventBaseCode VARCHAR(255) NOT NULL,
+    isRootEvent INTEGER NOT NULL,
+    quadClass INTEGER NOT NULL,
+    goldsteinScale REAL NOT NULL,
+    avgTone INTEGER NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_action_event_code ON 'Action' (eventCode);
+
+CREATE TABLE Geo (
+    id SERIAL PRIMARY KEY,
+    'type' INTEGER NOT NULL,
+    fullName VARCHAR(255) NOT NULL,
+    countryCode CHAR(2) NOT NULL,
+    adm1Code CHAR(4) NOT NULL,
+    adm2Code CHAR(5) NULL,
+    coordinates GEOGRAPHY NOT NULL,
+    featureID VARCHAR(255) NOT NULL
+);
+
+CREATE INDEX idx_geo_coordinates ON Geo USING GIST (coordinates);
+
+CREATE TABLE Actor (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(255) NOT NULL,
+    'name' VARCHAR(255) NOT NULL,
+    countryCode CHAR(3) NULL,
+    knownGroupCode VARCHAR(255) NULL,
+    ethnicCode VARCHAR(255) NULL,
+    religion1Code VARCHAR(255) NULL,
+    religion2Code VARCHAR(255) NULL,
+    type1Code VARCHAR(255) NULL,
+    type2Code VARCHAR(255) NULL,
+    type3Code VARCHAR(255) NULL,
+    geoID INTEGER NULL,
+
+    FOREIGN KEY geoID REFERENCES Geo (id)
+);
+
+CREATE UNIQUE INDEX idx_actor_code ON Actor (code);
+
+CREATE TABLE 'Event' (
+    globalEventID INTEGER PRIMARY KEY,
+    dateOccurred DATE NOT NULL,
+    actionID INTEGER NOT NULL,
+    actor1ID INTEGER NULL,
+    actor2ID INTEGER NULL,
+    geoID INTEGER NULL,
+
+    FOREIGN KEY actionID REFERENCES 'Action' (id),
+    FOREIGN KEY actor1ID REFERENCES Actor (id),
+    FOREIGN KEY actor2ID REFERENCES Actor (id),
+    FOREIGN KEY geoID REFERENCES Geo (id)
+);
+
+CREATE TABLE 'State' (
+    geoID INTEGER PRIMARY KEY,
+
+    FOREIGN KEY geoID REFERENCES Geo (id)
+);
+
+CREATE TABLE County (
+    geoID INTEGER PRIMARY KEY,
+    stateGeoID INTEGER,
+
+    FOREIGN KEY geoID REFERENCES Geo (id),
+    FOREIGN KEY stateGeoID REFERENCES 'State' (geoID)
+);
+
+CREATE TABLE SuicideRate (
+    id SERIAL PRIMARY KEY,
+    year INTEGER NOT NULL,
+    'population' INTEGER NOT NULL,
+    deaths INTEGER NOT NULL,
+    crudeRate REAL NULL,
+    ageAdjustedRate REAL NULL,
+    countyGeoID INTEGER NOT NULL,
+
+    FOREIGN KEY countyGeoID REFERENCES County (geoID) 
+);
+
+CREATE UNIQUE INDEX suicide_rate_county_year ON SuicideRate (year, countyGeoID);
+
+CREATE TABLE ElectionResult (
+    id SERIAL PRIMARY KEY,
+    year INTEGER NOT NULL,
+    votesDem INTEGER NOT NULL,
+    votesGOP INTEGER NOT NULL,
+    votesOther INTEGER NOT NULL,
+    countyGeoID INTEGER NOT NULL,
+
+    FOREIGN KEY countyGeoID REFERENCES County (geoID) 
+);
+
+CREATE UNIQUE INDEX election_result_county_year ON ElectionResult (year, countyGeoID);
