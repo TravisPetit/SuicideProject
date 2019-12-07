b1a3c6c9e169024035c33daa99d5e2baaee880f8
TravisPetit <rptravis96@gmail.com>
2019-12-07 21:22:57 +0100

    fix add_counties

--- sql_scripts/add_counties.sql
+++ sql_scripts/add_counties.sql
@@ -1,3 +1,17 @@
 set search_path to suicide_schema;
 
-insert into City (geoID, countyGID) select id gid from Geo join County on st_covers("geom"::geography, coordinates) where "type" = '3';
+DROP TABLE County;
+DROP TABLE City;
+
+ALTER TABLE "uscounties"
+RENAME TO County;
+
+CREATE TABLE City (
+    	geoID INTEGER PRIMARY KEY,
+   	countyGID INTEGER,
+
+    	FOREIGN KEY (geoID) REFERENCES Geo (id),
+    	FOREIGN KEY (countyGID) REFERENCES County (GID)
+);
+
+INSERT INTO City (geoID, countyGID) SELECT id gid FROM Geo join County ON st_covers("geom"::geography, coordinates) WHERE "type" = '3';
